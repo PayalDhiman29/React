165d111b59667fe725b580fa17d01e03147c9a37
PayalDhiman29 <pd3361333@gmail.com>
2025-08-22 19:34:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+const parent = React.createElement(
+    "div",
+
+    {id: "parent"},
+    React.createElement(
+        "div",
+        {id: "child"},
+        [React.createElement("h1", {}, "I am a child"), 
+            React.createElement("h2", {}, "I am another child"),]
+    )
+);
+// const heading = React.createElement("h1", 
+//     {id: "heading", xyz: "abc"}, 
+//     "Hello World from React!");
+
+    console.log(parent); // object
+
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(parent);
